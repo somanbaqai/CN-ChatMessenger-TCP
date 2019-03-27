e3a2cef5cf3519fd2a1013b6dee950b6d970c3af
somanbaqai <soman.baqai@gmail.com>
2019-03-27 17:45:22 +0500

    client msg remaining

---
+++ ChatMessengerClient/src/chatmessengerClient/ChatThreadSenderServer.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatmessengerClient;
+
+import ThreadFiles.*;
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author somanbaqai
+ */
+public class ChatThreadSenderServer implements Runnable {
+
+    String conn;
+    int port = 8001;
+    ServerSocket serverSocket ;//= new ServerSocket(port);
+    Socket socket;// = serverSocket.accept();
+
+    public ChatThreadSenderServer(String conn, ServerSocket serverSocket, Socket socket,int port) {
+        this.conn = conn;
+        this.serverSocket = serverSocket;
+        this.socket = socket;
+        this.port = port;
+    }
+
+    public ChatThreadSenderServer() {
+    }
+
+    public ChatThreadSenderServer(String conn,Socket socket) {
+        this.conn = conn;
+        this.socket = socket;
+    }
+
+   
+
+    @Override
+    public void run() {
+        try {
+            
+           // System.out.println( conn + " connection established");
+
+            DataInputStream dataInputStreamSen = new DataInputStream(System.in);
+            PrintStream printStream = new PrintStream(socket.getOutputStream());
+            while (true) {
+                System.out.print("Enter msg from " + conn + ": ");
+                
+                String strRec = dataInputStreamSen.readLine();
+                if(!strRec.isEmpty()){
+                    printStream.println(strRec);
+                }
+
+                if (strRec.equalsIgnoreCase("exit")) {
+                    serverSocket.close();
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
---
+++ ChatMessengerServer/src/chatmessengerServer/MainChatController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatmessengerServer;
+
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author somanbaqai
+ */
+public class MainChatController implements Initializable {
+
+    @FXML
+    private ListView <String> chatMessageList;
+    ObservableList<String> chatMessages = FXCollections.observableArrayList();
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TextField messagaeField;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        Thread recieverThread = new Thread(new ChatThreadRecieverServer("Client",MainController.port,MainController.socket));
+        recieverThread.start();
+        Thread senderThread = new Thread(new ChatThreadSenderServer("Client",MainController.socket));
+        senderThread.start();
+        chatMessageList.setItems(chatMessages);
+        
+    }    
+
+    @FXML
+    private void handleSendButton(ActionEvent event) {
+        chatMessages.add("User 1: " + messagaeField.getText());
+    }
+    
+}
